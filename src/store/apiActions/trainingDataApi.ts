972f2156fd28930146d913a1ef736913eb60b370
Abhay Tiwari <abhay90458@gmail.com>
2023-03-22 10:10:17 +0530

    Added headers in axios call for cors

--- src/store/apiActions/trainingDataApi.ts
+++ src/store/apiActions/trainingDataApi.ts
@@ -1,7 +1,12 @@
 import axios from 'axios';
 export const trainingDataApi = async () => {
-  return await axios
-    .get('https://ump-swift-backend-production-c0d4.up.railway.app/trainingInfo')
+  return await axios({
+    url: 'https://ump-swift-backend-production-c0d4.up.railway.app/trainingInfo',
+    method: 'get',
+    headers: {
+      'Access-Control-Allow-Origin': 'https://main--stellular-maamoul-1bdee3.netlify.app/',
+    },
+  })
     .then(response => response.data) // ADD toast
     .catch(e => console.log('error in caaling user data', e));
 };
--- src/store/apiActions/usersDataApi.ts
+++ src/store/apiActions/usersDataApi.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 
 export const usersDataApi = async () => {
-  return await axios
-    .get('https://ump-swift-backend-production-c0d4.up.railway.app/users')
+  return await axios({
+    url: 'https://ump-swift-backend-production-c0d4.up.railway.app/users',
+    method: 'get',
+    headers: {
+      'Access-Control-Allow-Origin': 'https://main--stellular-maamoul-1bdee3.netlify.app/',
+    },
+  })
     .then(response => response.data)
     .catch(e => console.log('error in calling user data', e));
 };
