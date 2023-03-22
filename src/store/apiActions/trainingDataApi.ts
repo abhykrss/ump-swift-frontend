38ed0d25d7cc2adad506c5d5fea2450acc203a12
Abhay Tiwari <abhay90458@gmail.com>
2023-03-22 09:59:14 +0530

    removed Proxy

--- src/store/apiActions/trainingDataApi.ts
+++ src/store/apiActions/trainingDataApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const trainingDataApi = async () => {
   return await axios
-    .get('/trainingInfo')
+    .get('https://ump-swift-backend-production-c0d4.up.railway.app/trainingInfo')
     .then(response => response.data) // ADD toast
     .catch(e => console.log('error in caaling user data', e));
 };
