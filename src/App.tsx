9dc9ba935faf7a448c2790653e970a4e0edbc15e
shadaf266 <shadafsiddiqui5@gmail.com>
2023-03-23 18:19:52 +0530

    update ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import './App.css';
 import AppRoutes from './routes/AppRoutes';
 
 export const config = {
-  endpoint: process.env.REACT_APP_SERVER_URL,
+  endpoint: 'https://ump-swift-backend-production-c0d4.up.railway.app/',
 };
 function App() {
   return (
